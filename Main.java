fbdbbe113e7f851c0de1526e5e195e235cd09eac
ahsnghifari <ahsanghifari04@gmail.com>
2023-10-11 19:26:00 +0700

    Bljr Java

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // atribute
+        int a, b;
+        int hasilJumlah, hasilKurang, hasilKali;
+        a = 10;
+        b = 20;
+        // konstruktor
+        hasilJumlah = a + b;
+        hasilKurang = a - b;
+        hasilKali = a * b;
+        System.out.print(
+                "\nHasil Semua Perhitungan adalah \t" + "\nJumlah\t=" + hasilJumlah + "\nKurang\t=" + hasilKurang
+                        + "\nKali\t=" + hasilKali);
     }
 }
